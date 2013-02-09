ff0a9e86dd66179e3f145673915dfecf3c06f9f7
Burt Beckwith <burt@burtbeckwith.com>
2013-02-09 00:45:06 -0500

    updated to the latest version of the release plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -17,8 +17,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(":tomcat:$grailsVersion",
-              ":release:1.0.0") {
+        build ':release:2.2.0', ':rest-client-builder:1.0.3', {
             export = false
         }
 
