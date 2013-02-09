f7de63a52f6e47f405d0a50c12317888eddaa668
Burt Beckwith <burt@burtbeckwith.com>
2013-02-09 00:43:58 -0500

    updated license, issueManagement, and scm properties

--- RecaptchaSpringSecurityGrailsPlugin.groovy
+++ RecaptchaSpringSecurityGrailsPlugin.groovy
@@ -11,22 +11,11 @@ class RecaptchaSpringSecurityGrailsPlugin {
 
     def documentation = "http://grails.org/plugin/recaptcha-spring-security"
 
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
+    def license = "APACHE"
 //    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-    // Any additional developers beyond the author specified above.
 //    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-    // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.grails-plugins.codehaus.org/browse/grails-plugins/" ]
+    def issueManagement = [system: "Github", url: "https://github.com/rpalcolea/recaptcha-spring-security/issues"]
+    def scm = [url: "https://github.com/rpalcolea/recaptcha-spring-security"]
 
     def doWithSpring = {
         // TODO Implement runtime spring config (optional)
